f335d2e32854b80147bf41ca81220b837b8b55d0
Julien Deveaux <julien.deveaux@hotmail.com>
2022-03-24 00:01:18 +0100

    Bon affichage des détails raw

--- src/data.js
+++ src/data.js
@@ -4,16 +4,15 @@ import React from "react";
 class Data extends React.Component {
     constructor(props) {
         super(props);
-        this.json = '';
     }
 
-    displayDetails() {
+    displayType() {
         if(this.props.Parentjson !== '') {
             let json = '';
             try {
                 json = JSON.parse(this.props.Parentjson);
                 let item = json.find(e => e.id === this.props.idSelected);
-                return <div>{item.type}</div>;
+                return <h2>Type : {item.type}</h2>;
             } catch (error) {
             }
         }
@@ -31,11 +30,54 @@ class Data extends React.Component {
         }
     }
 
+    displayArray() {
+        if(this.props.Parentjson !== '') {
+            let json = '';
+            try {
+                json = JSON.parse(this.props.Parentjson);
+                console.log(json)
+                let item = json.find(e => e.id === this.props.idSelected);
+                if(item.data.hasOwnProperty("value")) {
+                    let status = "";
+                    if(item.type === "DOOR") {
+                        status = item.data.value === 1 ? "OPEN" : "CLOSED";
+                    } else if(item.type === "SWITCH" || item.type === "LIGHT") {
+                        status = item.data.value === 1 ? "ON" : "OFF";
+                    }
+                    return <h2>Status : {status}</h2>
+                } else {
+                    let myFormatedData = [];
+                    let id = 0;
+                    for(let i = 0; i < item.data.values.length; i++) {
+                        myFormatedData[i] = ({
+                            id: id,
+                            label: item.data.labels[i],
+                            data: item.data.values[i]
+                        });
+                        if (item.type === "HUMIDITY") {
+                            myFormatedData[i].data = item.data.values[i] + "%"
+                        } else if(item.type === "TEMPERATURE") {
+                            myFormatedData[i].data = item.data.values[i] + "°"
+                        } else if(item.type === "FAN_SPEED") {
+                            myFormatedData[i].data = item.data.values[i] + "rpm"
+                        }
+                        id++;
+                    }
+                    return <table><tbody>
+                    {myFormatedData.map(e => <tr key={e.id}><th>{e.label}</th><th>{e.data}</th></tr>)}
+                    </tbody></table>;
+                }
+            } catch (error) {
+            }
+        }
+    }
+
     render() {
         return (
             <div>
                 {this.displayTitle()}
-                {this.displayDetails()}
+                {this.displayType()}
+                {this.displayArray()}
             </div>
         );
     }
