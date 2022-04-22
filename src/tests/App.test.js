4acbb5f4f567ebfbf2440f36a654f54b399efd51
Julien Deveaux <julien.deveaux@hotmail.com>
2022-04-22 15:10:30 +0200

    Add - Test général

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,8 +1,42 @@
-import { render, screen } from '@testing-library/react';
+import {act, fireEvent, render} from '@testing-library/react';
 import App from '../App';
+import {unmountComponentAtNode} from "react-dom";
 
 describe("Tests de la racine + récupération json par fetch", () => {
-  it("récupération du json par fetch", () => {
+    let container = null;
 
-  });
+    beforeEach(() => {
+        container = document.createElement("div");
+        document.body.appendChild(container);
+    });
+
+    afterEach(() => {
+        unmountComponentAtNode(container);
+        container.remove();
+        container = null;
+        jest.useRealTimers();
+    });
+
+    it("Test navigation", () => {
+        act(() => {
+            render(
+                <App/>, container
+            );
+        });
+        let textInput = document.getElementsByTagName("input");
+        fireEvent.change(textInput.item(0), {target: {value: "/data.json"}});
+        setTimeout(() => {          //attends la récupération du json et le parse (0.5s)
+            const sensorList = document.getElementsByTagName("a");
+            expect(sensorList.length).toBe(11);
+            act(() => {
+                sensorList[0].dispatchEvent(new MouseEvent('click', {bubbles: true}));
+            });
+            let title = document.getElementsByTagName("h1");
+            let type = document.getElementsByTagName("h2");
+            let status = document.getElementsByTagName("h3");
+            expect(title).toBe("Température Bureau");
+            expect(type).toBe("TEMPERATURE");
+            expect(status).toBe("");
+        }, 50000);
+    });
 })
